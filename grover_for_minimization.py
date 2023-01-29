3e593ff44bd7e2835e520a033c7972b2483518cd
Michael Luciuk <mrl280@mail.usask.ca>
2023-01-29 02:45:07 -0500

    We have a complete circuit, however it remains untested.

--- grover_for_minimization.py
+++ grover_for_minimization.py
@@ -10,10 +10,10 @@ L. Grover, “A fast quantum mechanical algorithm for database search,” in Pro
 """
 import math
 
-import numpy as np
+import qiskit
 from qiskit import ClassicalRegister, QuantumCircuit, QuantumRegister
-from qiskit.circuit.library import GroverOperator, HGate
-from qiskit.providers.aer import QasmSimulator
+from qiskit.circuit.library import GroverOperator
+from qiskit import Aer
 from qiskit.visualization import plot_histogram
 
 import matplotlib.pyplot as plt
@@ -24,36 +24,28 @@ from typing import List
 from qft import qft
 
 
-def minimization_oracle(circuit: QuantumCircuit, data_register_q: QuantumRegister,
-                        ancillary_register_q: QuantumRegister, arr: List[int], x: int) -> QuantumCircuit:
+def minimization_oracle(arr: List[int], x: int) -> QuantumCircuit:
     """
     Marking oracle that flips the sign of elements that satisfy the minimization condition.
         That is: mark states |i> (take |i> to -|i> if arr[i] < x).
 
-    :param circuit:
-    :param data_register_q:
-    :param ancillary_register_q:
-    :param arr:
-    :param x:
-    :return:
+    :param arr: List of ints:
+        The list we are searching.
+    :param x: int:
+        Our current pivot element.
+
+    :return: QuantumCircuit:
+        A marking oracle.
     """
     # We need one wire in the data register for each element of arr.
     number_of_data_qubits_required = len(arr)
     data_register_q = QuantumRegister(number_of_data_qubits_required, 'data_q')
-
-    # Measurment results from the data register will go here.
-    data_register_c = ClassicalRegister(number_of_data_qubits_required, "data_c")
 
     # We need enough ancillary qubits to represent 0 -> sum(arr) or x, whichever is larger.
     #  Recall you can represent up to 2^n - 1 with n bits.
     number_ancillary_qubits_required = max(math.ceil(math.log(sum(arr) + 1, 2)), math.ceil(math.log(x + 1, 2)))
     ancillary_register_q = QuantumRegister(number_ancillary_qubits_required, 'ancillary_q')
     # Ancillary bits are just work bits, we won't need to measure them.
-
-    print("Data Register:")
-    print(data_register_q)
-    print("Ancillary Register:")
-    print(ancillary_register_q)
 
     oracle_circuit = QuantumCircuit(ancillary_register_q, data_register_q)
 
@@ -80,8 +72,8 @@ def minimization_oracle(circuit: QuantumCircuit, data_register_q: QuantumRegiste
         if binary_list[-1] == 0:
             sign_flip_circuit.x(ancillary_register_q_for_flip[-1])
 
+        # Since we don't have a controlled Z-gate, use the identity HXH = Z.
         sign_flip_circuit.h(ancillary_register_q_for_flip[-1])
-
         sign_flip_circuit.mcx(control_qubits=ancillary_register_q_for_flip[:-1],
                               target_qubit=ancillary_register_q_for_flip[-1])
         sign_flip_circuit.h(ancillary_register_q_for_flip[-1])
@@ -89,15 +81,26 @@ def minimization_oracle(circuit: QuantumCircuit, data_register_q: QuantumRegiste
         if binary_list[-1] == 0:
             sign_flip_circuit.x(ancillary_register_q_for_flip[-1])
 
-        circuit = circuit.compose(sign_flip_circuit)
+        oracle_circuit = oracle_circuit.compose(sign_flip_circuit)
 
-    circuit = circuit.compose(oracle_circuit_for_cleanup)  # Cleanup.
-    return circuit
+    oracle_circuit = oracle_circuit.compose(oracle_circuit_for_cleanup)  # Cleanup.
+    return oracle_circuit
 
 
 def load_values_in_arr(circuit: QuantumCircuit, n_ancillary_qubits: int, n_data_qubits: int, arr: List[int]):
     """
     Conditionally load all the values in arr onto the axcillary register.
+
+    :param circuit: QuantumCircuit:
+    :param n_ancillary_qubits: int:
+        The number of ancillary qubits.
+    :param n_data_qubits: int:
+        The number of data qubits wires.
+    :param arr: List of ints:
+        The list we are searching.
+    :return: QuantumCircuit:
+        The provided circuit, except with added gates to conditionally load all the values in arr onto the
+         ancillary register
     """
     circuit_for_recovery = circuit.copy()
 
@@ -106,18 +109,14 @@ def load_values_in_arr(circuit: QuantumCircuit, n_ancillary_qubits: int, n_data_
 
     # Loop through each of the data wires, preforming controlled additions.
     for j in range(n_data_qubits):
-        print(j)
         data_reg_q = QuantumRegister(n_data_qubits, 'data_q')
         ancillary_reg_q = QuantumRegister(n_ancillary_qubits, 'ancillary_q')
 
         k_addition_circuit = QuantumCircuit(ancillary_reg_q, data_reg_q)
         for n in range(n_ancillary_qubits):
             k_addition_circuit.crz(arr[j] * pi / (2 ** j), n_ancillary_qubits + j, n)
-        # custom_rotation = k_addition_circuit.to_gate().control(num_ctrl_qubits=1)
-        circuit = circuit.compose(k_addition_circuit)
 
-        # controlled_rotation = add_k_fourier(k=arr[j], wires=ancillary_register_q).control(2)
-        # minimization_oracle.ctrl(op=add_k_fourier(k=arr[j], wires=ancillary_register_q), control=data_register_q[j])
+        circuit = circuit.compose(k_addition_circuit)
 
     # Return to the computational basis.
     adjoint_circuit = qft(circuit=circuit_for_recovery, n=n_ancillary_qubits).inverse()
@@ -129,6 +128,7 @@ def load_values_in_arr(circuit: QuantumCircuit, n_ancillary_qubits: int, n_data_
 def to_list(n, n_wires):
     """
     This function is copy and pasted straight out of PennyLane!
+
     Convert an integer into a binary integer list
     Args:
         n (int): Basis state as integer
@@ -138,7 +138,6 @@ def to_list(n, n_wires):
     Returns:
         (array[int]): integer binary array
     """
-    # From PennyLane
     if n >= 2 ** n_wires:
         raise ValueError(f"cannot encode {n} with {n_wires} wires ")
 
@@ -149,55 +148,55 @@ def to_list(n, n_wires):
 
 def minimization_circuit(arr: List[int], x: int) -> QuantumCircuit:
     """
-    Build a quantum minimization circuit.
+    Build the complete quantum minimization circuit to find if there exists an element in arr < x.
 
-    :param arr: list of ints.
+    :param arr: List of ints.
     :param x: int.
 
     :return: Minimization circuit.
     """
     # We need one wire in the data register for each element of arr.
     number_of_data_qubits_required = len(arr)
-    data_register_q = QuantumRegister(number_of_data_qubits_required, 'data_q')
+    quantum_data_register = QuantumRegister(number_of_data_qubits_required, 'data_q')
 
-    # Measurment results from the data register will go here.
-    data_register_c = ClassicalRegister(number_of_data_qubits_required, "data_c")
+    # Measurment results from the quantum data register will be projected onto classical wires.
+    classical_data_register = ClassicalRegister(number_of_data_qubits_required, "data_c")
 
     # We need enough ancillary qubits to represent 0 -> sum(arr) or x, whichever is larger.
     #  Recall you can represent up to 2^n - 1 with n bits.
     number_ancillary_qubits_required = max(math.ceil(math.log(sum(arr) + 1, 2)), math.ceil(math.log(x + 1, 2)))
-    ancillary_register_q = QuantumRegister(number_ancillary_qubits_required, 'ancillary_q')
+    quantum_ancillary_register = QuantumRegister(number_ancillary_qubits_required, 'ancillary_q')
     # Ancillary bits are just work bits, we won't need to measure them.
 
-    print(data_register_q)
-    print(ancillary_register_q)
+    # Create a quantum circuit
+    circuit = QuantumCircuit(quantum_ancillary_register, quantum_data_register, classical_data_register)
 
-    # Create a Quantum Circuit
-    circuit = QuantumCircuit(data_register_q, ancillary_register_q, data_register_c)
-
+    # Grover's Search, here we go..!
     # Step 1: Create an equal superposition by applying a Hadamard to each wire in the data register.
-    for wire in data_register_q:
+    for wire in quantum_data_register:
         circuit.h(wire)
 
-    circuit.barrier(data_register_q)
+    circuit.barrier(quantum_data_register)  # Just to ease of visulation.
 
-    # Upon measurment at the
-    j = 1
+    # The optimal number of iterations requires knowledge of the number of marked elements.
+    # Alternatively, we could assume a minimum number of marked elements and work our way up.
+    j = 2
     for _ in range(j):
-        # Step 2: Use the oracle to mark solution states.
+        # Step 2: Get an oracle we can use to build quantum states.
+        oracle = minimization_oracle(arr=arr, x=x)
+
         # Step 3: Apply the Grover operator to amplify the probability of getting the correct solution.
-        oracle = minimization_oracle(circuit=circuit, data_register_q=data_register_q,
-                                     ancillary_register_q=ancillary_register_q, arr=arr, x=x)
-
-        # Draw the circuit
-        oracle.draw(output='mpl')
-        plt.show()
-
         grover_op = GroverOperator(oracle=oracle, insert_barriers=True)
 
         circuit = circuit.compose(grover_op)
 
-    circuit.measure(data_register_q, data_register_c)
+    # Draw the minimization oracle
+    oracle.draw(output='mpl')
+    plt.show()
+
+    circuit.barrier(quantum_data_register)  # Just to ease of visulation.
+
+    # circuit.measure(quantum_data_register, classical_data_register)
 
     return circuit
 
@@ -214,6 +213,12 @@ def grover_for_minimization(arr: List[int], x: int) -> bool:
         False: otherwise.
     """
     circuit = minimization_circuit(arr=arr, x=x)
+
+    backend = Aer.get_backend("qasm_simulator")
+    job = qiskit.execute(experiments=circuit, backend=backend, shots=1)
+    counts = job.result().get_counts()
+
+    print(counts)
 
     # Draw the circuit
     circuit.draw(output='mpl')
