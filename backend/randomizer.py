f78edf426a1faf5551b274ff874c540847675f37
Michael Luciuk <mrl280@mail.usask.ca>
2023-01-29 06:10:34 -0500

    Docstring templates

--- backend/randomizer.py
+++ backend/randomizer.py
@@ -1,14 +1,32 @@
+"""
+Author: AmplifiQation
+
+iQuHACK 2023
+
+# TODO:
+"""
+
+
 import os
 import qiskit as qt
 from qiskit import QuantumCircuit, Aer
 from qiskit import IBMQ
-import covalent as ct
+# import covalent as ct
+
 
 class RNG:
+    """
+    # TODO
+    """
     # _backend: 0 for local simulator, 1 for IBM sim, 2 for QC
     backend: str
 
     def __init__(self, backend: int, token) -> None:
+        """
+        # TODO
+        :param backend:
+        :param token:
+        """
         IBMQ.save_account(token)
         IBMQ.load_account()
         provider = IBMQ.get_provider(hub='ibm-q')
@@ -19,9 +37,13 @@ class RNG:
         else:
             self.backend = Aer.get_backend('qasm_simulator')
 
-    @ct.electron
+    # @ct.electron
     def randomizer_circuit(self, num_qubits: int) -> int:
-        """Returns a rand int based on num_qubits available"""
+        """
+        Returns a random integer based on the number of qubits available.
+        :param num_qubits:
+        :return:
+        """
         circuit = QuantumCircuit(num_qubits, num_qubits)
         for i in range(num_qubits):
             circuit.h(i)
